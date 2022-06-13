a192bad21b49244f18dd9c1107156fbec71249f2
vincius <vinisurf1206@gmail.com>
2022-06-13 20:27:07 -0300

    tarefa concluida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhpar = (numeroDigitado % 2) == 0;
+
+if (numeroEhpar)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine($"{numeroDigitado} é par");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine($"{numeroDigitado} é ímpar");
+}
+Console.ResetColor();
